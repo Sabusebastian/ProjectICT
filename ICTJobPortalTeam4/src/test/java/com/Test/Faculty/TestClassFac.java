5aee68996007c1b0aa869e1548806e8493797244
Sabu Sebastian <sabusebastian19@gmail.com>
2023-02-25 23:23:19 +0530

    Demo Commit

--- ICTJobPortalTeam4/src/test/java/com/Test/Faculty/TestClassFac.java
+++ ICTJobPortalTeam4/src/test/java/com/Test/Faculty/TestClassFac.java
@@ -12,47 +12,51 @@ import com.Pages.Faculty.SignupFac;
 import com.Utilities.ExcelUtility;
 
 public class TestClassFac extends BaseClass {
-SignupFac objSignup;
-LoginpageFac objLog;
-SignoutFac objSignout;
+	SignupFac objSignup;
+	LoginpageFac objLog;
+	SignoutFac objSignout;
 
-@Test(priority=1)
-public void Signupverification() throws InterruptedException, IOException {
-	
+	@Test(priority = 1)
+	public void Signupverification() throws InterruptedException, IOException {
 
-objSignup=new SignupFac(driver);
-objSignup.clickonloginasFaculty();
-objSignup.clickonSignup();
-Object enterName=ExcelUtility.GetCellData(1, 3,System.getProperty("user.dir")+ "\\src\\main\\resources\\ExcelHome.xlsx", 0);
-Object enterEmail=ExcelUtility.GetCellData(1, 1, System.getProperty("user.dir")+"\\src\\main\\resources\\ExcelHome.xlsx", 0);
-Object enterPassword=ExcelUtility.GetCellData(1,2, System.getProperty("user.dir")+"\\src\\main\\resources\\ExcelHome.xlsx", 0);
+		objSignup = new SignupFac(driver);
+		objSignup.clickonloginasFaculty();
+		objSignup.clickonSignup();
+		Object enterName = ExcelUtility.GetCellData(1, 3,
+				System.getProperty("user.dir") + "\\src\\main\\resources\\ExcelHome.xlsx", 0);
+		Object enterEmail = ExcelUtility.GetCellData(1, 1,
+				System.getProperty("user.dir") + "\\src\\main\\resources\\ExcelHome.xlsx", 0);
+		Object enterPassword = ExcelUtility.GetCellData(1, 2,
+				System.getProperty("user.dir") + "\\src\\main\\resources\\ExcelHome.xlsx", 0);
 
-objSignup.enterName(enterName);
-objSignup.enterEmail(enterEmail);
-objSignup.enterPassword(enterPassword);
-objSignup.clicksubmit();
-String actualUrl=driver.getCurrentUrl();
+		objSignup.enterName(enterName);
+		objSignup.enterEmail(enterEmail);
+		objSignup.enterPassword(enterPassword);
+		objSignup.clicksubmit();
+		String actualUrl = driver.getCurrentUrl();
 
-String expectedUrl="https://delightful-earrings-fish.cyclic.app/faculty";
-Assert.assertEquals(actualUrl, expectedUrl,"expected Url and actual Url are not same");
-System.out.println("Test Finished");
-}
+		String expectedUrl = "https://delightful-earrings-fish.cyclic.app/faculty";
+		Assert.assertEquals(actualUrl, expectedUrl, "expected Url and actual Url are not same");
+		System.out.println("SignupVerfication - Test Finished ");
+	}
 
-@Test(priority=2)
-public void Loginverification() throws IOException, InterruptedException {
-	objLog=new LoginpageFac(driver);
-	
-	Object enterEmail=ExcelUtility.GetCellData(2, 1,System.getProperty("user.dir")+"\\src\\main\\resources\\ExcelHome.xlsx", 0);
-	Object enterPassword=ExcelUtility.GetCellData(2, 2,System.getProperty("user.dir")+"\\src\\main\\resources\\ExcelHome.xlsx", 0);
-	objLog.enterEmail(enterEmail);
-	objLog.enterPassword(enterPassword);
-	
-	
-	objLog.clicksubmit();
-}
+	@Test(priority = 2)
+	public void Loginverification() throws IOException, InterruptedException {
+		objLog = new LoginpageFac(driver);
 
-@Test(priority=3)
-public void Logoutverification()
-{objSignout=new SignoutFac(driver);
-	objSignout.clicksignout();}
+		Object enterEmail = ExcelUtility.GetCellData(2, 1,
+				System.getProperty("user.dir") + "\\src\\main\\resources\\ExcelHome.xlsx", 0);
+		Object enterPassword = ExcelUtility.GetCellData(2, 2,
+				System.getProperty("user.dir") + "\\src\\main\\resources\\ExcelHome.xlsx", 0);
+		objLog.enterEmail(enterEmail);
+		objLog.enterPassword(enterPassword);
+
+		objLog.clicksubmit();
+	}
+
+	@Test(priority = 3)
+	public void Logoutverification() {
+		objSignout = new SignoutFac(driver);
+		objSignout.clicksignout();
+	}
 }
